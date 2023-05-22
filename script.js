7bee67cfb20820db02d6604a2b95edf634476817
Madhab Tripathy <66151264+madhab-tripathy@users.noreply.github.com>
2023-05-22 18:42:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
 //your JS code here. If required.
+const browser = document.getElementById("browser-info");
+const info = `You are using ${window.navigator.appName} version ${window.navigator.appVersion}`
+browser.innerText = info
